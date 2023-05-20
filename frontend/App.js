1ced16524ae7c5f53bcdd7546ac195bc3ceb17e2
Vlad <vlad@thesixbysix.co.uk>
2023-05-21 01:16:30 +0300

    reformatted code

--- frontend/App.js
+++ frontend/App.js
@@ -1,29 +1,28 @@
 import React from 'react';
-import {StatusBar, Text, View,StyleSheet} from 'react-native';
+import {StatusBar, Text, View, StyleSheet} from 'react-native';
 import Navigation from './src/components/Navigation';
-import { PaperProvider } from 'react-native-paper';
+import {PaperProvider} from 'react-native-paper';
 import {AuthProvider} from './src/context/AuthContext';
 
 
-
 const App = () => {
-  return (
-    <PaperProvider >
-      <AuthProvider>
-        <StatusBar backgroundColor="#06bcee" />
-        <Navigation />
-      </AuthProvider>
-    </PaperProvider>
-  );
+    return (
+        <PaperProvider>
+            <AuthProvider>
+                <StatusBar backgroundColor="#06bcee"/>
+                <Navigation/>
+            </AuthProvider>
+        </PaperProvider>
+    );
 };
 
 
 export default App;
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -1,43 +1,36 @@
-import React ,{useContext} from 'react';
+import React, {useContext} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Feed from '../screens/Feed';
 import LoginScreen from '../screens/Loginsecreen';
 import SplashScreen from '../screens/SplashScreen';
 import RegisterScreen from '../screens/Registersecreen';
-import { AuthContext } from '../context/AuthContext.js';
+import {AuthContext} from '../context/AuthContext.js';
+
 const Stack = createNativeStackNavigator();
 
 const Navigation = () => {
-  const {userInfo, splashLoading} = useContext(AuthContext);
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-      {splashLoading ? (
-          <Stack.Screen
-            name="Splash Screen"
-            component={SplashScreen}
-            options={{headerShown: false}}
-          />
-          ) : userInfo.token ? (
-          <Stack.Screen name="Home" component={Feed} />
-          ) : (
-            <>
-            <Stack.Screen
-              name="Login"
-              component={LoginScreen}
-              options={{headerShown: false}}
-            />
-            <Stack.Screen
-              name="Register"
-              component={RegisterScreen}
-              options={{headerShown: false}}
-            />
-            </>
-          )} 
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+    const {userInfo, splashLoading} = useContext(AuthContext);
+    return (<NavigationContainer>
+        <Stack.Navigator>
+            {splashLoading ? (<Stack.Screen
+                name="Splash Screen"
+                component={SplashScreen}
+                options={{headerShown: false}}
+            />) : userInfo.token ? (<Stack.Screen name="Home" component={Feed}/>) : (<>
+                <Stack.Screen
+                    name="Login"
+                    component={LoginScreen}
+                    options={{headerShown: false}}
+                />
+                <Stack.Screen
+                    name="Register"
+                    component={RegisterScreen}
+                    options={{headerShown: false}}
+                />
+            </>)}
+        </Stack.Navigator>
+    </NavigationContainer>);
 };
 
 export default Navigation;
