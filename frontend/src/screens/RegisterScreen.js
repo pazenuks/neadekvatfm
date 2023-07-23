66c97c0d73b0678ddc8724b897304076f3feac4b
Vlad <vlad@thesixbysix.co.uk>
2023-07-23 20:55:23 +0300

    swipe to login

--- frontend/src/screens/RegisterScreen.js
+++ frontend/src/screens/RegisterScreen.js
@@ -11,6 +11,8 @@ import {
 import client from '../config';
 import Spinner from 'react-native-loading-spinner-overlay';
 import {AuthContext} from '../context/AuthContext.js';
+import {PanGestureHandler, State} from "react-native-gesture-handler";
+import {DrawerActions} from "@react-navigation/native";
 
 const RegisterScreen = ({navigation}) => {
     const [name, setName] = useState(null);
@@ -20,58 +22,66 @@ const RegisterScreen = ({navigation}) => {
     const {isLoading, register, error} = useContext(AuthContext);
     const image = {uri: "https://img.freepik.com/free-vector/abstract-shiny-grey-technology-background_1035-12620.jpg?w=740&t=st=1667419101~exp=1667419701~hmac=3bbdef34e890179fbe282cbbf64169f4f1d670dcc98086340713541f09d6ac23"};
 
+    const handleGestureEvent = ({ nativeEvent }) => {
+        if (nativeEvent.state === State.END && nativeEvent.translationX >= 50) {
+            navigation.navigate('Login');
+        }
+    };
+
     return (
-        <View style={styles.container}>
-            <Spinner visible={isLoading}/>
-            <ImageBackground source={image} resizeMode="cover" style={styles.image}>
-                <Text style={styles.title}>Sing Up</Text>
-                <View style={styles.wrapper}>
-                    <TextInput
-                        style={styles.input}
-                        value={name}
-                        placeholder="Enter name"
-                        onChangeText={text => setName(text)}
-                    />
+        <PanGestureHandler onHandlerStateChange={handleGestureEvent}>
+            <View style={styles.container}>
+                <Spinner visible={isLoading}/>
+                <ImageBackground source={image} resizeMode="cover" style={styles.image}>
+                    <Text style={styles.title}>Sing Up</Text>
+                    <View style={styles.wrapper}>
+                        <TextInput
+                            style={styles.input}
+                            value={name}
+                            placeholder="Enter name"
+                            onChangeText={text => setName(text)}
+                        />
 
-                    <TextInput
-                        style={styles.input}
-                        value={email}
-                        placeholder="Enter email"
-                        onChangeText={text => setEmail(text)}
-                    />
+                        <TextInput
+                            style={styles.input}
+                            value={email}
+                            placeholder="Enter email"
+                            onChangeText={text => setEmail(text)}
+                        />
 
-                    <TextInput
-                        style={styles.input}
-                        value={password}
-                        placeholder="Enter password"
-                        onChangeText={text => setPassword(text)}
-                        secureTextEntry
-                    />
+                        <TextInput
+                            style={styles.input}
+                            value={password}
+                            placeholder="Enter password"
+                            onChangeText={text => setPassword(text)}
+                            secureTextEntry
+                        />
 
-                    <TextInput
-                        style={styles.input}
-                        value={Confpassword}
-                        placeholder="confirmer password"
-                        onChangeText={text => setConfPassword(text)}
-                        secureTextEntry
-                    />
+                        <TextInput
+                            style={styles.input}
+                            value={Confpassword}
+                            placeholder="confirmer password"
+                            onChangeText={text => setConfPassword(text)}
+                            secureTextEntry
+                        />
 
-                    <Button
-                        title="Register"
-                        onPress={() => {
-                            register(name, email, password, Confpassword, navigation);
-                        }}
-                    />
+                        <Button
+                            title="Register"
+                            onPress={() => {
+                                register(name, email, password, Confpassword, navigation);
+                            }}
+                        />
 
-                    <View style={{flexDirection: 'row', marginTop: 20}}>
-                        <Text>Already have an accoutn? </Text>
-                        <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Login')}>
-                            <Text style={styles.link}>Login</Text>
-                        </TouchableOpacity>
+                        <View style={{flexDirection: 'row', marginTop: 20}}>
+                            <Text>Already have an accoutn? </Text>
+                            <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Login')}>
+                                <Text style={styles.link}>Login</Text>
+                            </TouchableOpacity>
+                        </View>
                     </View>
-                </View>
-            </ImageBackground>
-        </View>
+                </ImageBackground>
+            </View>
+        </PanGestureHandler>
     );
 };
 
